d0e778bac837f1161bc76c3ea679109541f6837c
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-11-06 19:18:18 +0300

    first_Stack

--- AwardsCollectionApp/FigureView.swift
+++ AwardsCollectionApp/FigureView.swift
@@ -9,19 +9,45 @@ import SwiftUI
 
 struct FigureView: View {
     var body: some View {
-        Circle()
-            .fill(
-            LinearGradient(
-                gradient: Gradient(colors: [.pink, .yellow]),
-            startPoint: UnitPoint(x: 0, y: 1),
-            endPoint:  UnitPoint(x: 1, y: 0)
-            )
-            )
+        ZStack{
+            VStack{
+                VStack{
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.pink, .yellow]),
+                                startPoint: UnitPoint(x: 0, y: 1),
+                                endPoint:  UnitPoint(x: 1, y: 0)
+                            )
+                        )
+                    
+                }
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.pink, .yellow]),
+                            startPoint: UnitPoint(x: 0, y: 1),
+                            endPoint:  UnitPoint(x: 1, y: 0)
+                        )
+                    )
+                
+            }
+            
+            Circle()
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [.pink, .yellow]),
+                        startPoint: UnitPoint(x: 0, y: 1),
+                        endPoint:  UnitPoint(x: 1, y: 0)
+                    )
+                )
+        }
     }
 }
-
+    
 struct FigureView_Previews: PreviewProvider {
     static var previews: some View {
         FigureView()
     }
 }
+
